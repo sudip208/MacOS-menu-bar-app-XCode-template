3381c1c98407c787fc46c52b0bddc6a1d373847f
the-trumpeter <gill.palmer@education.nsw.gov.au>
2025-08-29 10:35:45 +1000

    Initial Commit

---
+++ menuBarAppTemplate/menuBarAppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuBarAppTemplateApp.swift
+//  menuBarAppTemplate
+//
+//  Created by Gill Palmer on 29/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct menuBarAppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
